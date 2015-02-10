b86e6edbc9d8726efdee2994e7a5dcfe42b3a79d
Tom Black <blacktm@gmail.com>
2015-02-09 23:07:21 -0500

    Updating `triangle.c` to reflect API changes

--- tests/triangle.c
+++ tests/triangle.c
@@ -5,15 +5,15 @@ Window *window;
 void render() {
   S2D_DrawTriangle(
     320,  50, 1, 0, 0, 1,
-    100, 430, 0, 1, 0, 1,
-    540, 430, 0, 0, 1, 1
+    540, 430, 0, 1, 0, 1,
+    100, 430, 0, 0, 1, 1
   );
 }
 
 int main(int argc, char const *argv[]) {
   
   window = S2D_CreateWindow(
-    "Hello Triangle", 640, 480, NULL, render, NULL, NULL
+    "Hello Triangle", 640, 480, NULL, render
   );
   
   S2D_Show(window);
